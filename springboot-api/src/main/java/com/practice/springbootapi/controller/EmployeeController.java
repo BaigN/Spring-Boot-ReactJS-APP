2cd635bef6df7323d86ca1ef9e7321a168992cbc
Nadeem Baig <n.baig90@gmail.com>
2020-09-06 22:53:19 -0500

    updated spring datasource propeties and added employee get endpoint

---
+++ springboot-api/src/main/java/com/practice/springbootapi/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.practice.springbootapi.controller;
+
+import com.practice.springbootapi.model.Employee;
+import com.practice.springbootapi.repository.EmployeeRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  // get all employees
+  @GetMapping("/employees")
+  public List<Employee> getAllEmployees() {
+    return employeeRepository.findAll();
+  }
+
+}
